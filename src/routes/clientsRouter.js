6849779c384e69cdbf88b00ff4a31a5b82e2d9ae
SHEDRACK ALITA <davisshedrack3308@gmail.com>
2025-05-28 17:05:12 +0100

    Update clientsRouter.js

--- src/routes/clientsRouter.js
+++ src/routes/clientsRouter.js
@@ -1,16 +1,11 @@
 
 const clientCont = require ('../controller/clientsController');
-
 const express = require ('express');
-
 const route = express.Router();
 
 route.get('/clients', clientCont.getclients);
-
 route.get('/client', clientCont.getClient);
-
 route.post('/signUp', clientCont.postedClient);
-
 route.delete('./delete', clientCont.deleteClient);
 
 module.export = route;
